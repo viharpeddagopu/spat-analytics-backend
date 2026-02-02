b989b7b3d6366fab17c911167ca201d451fe1dd6
Peddagopu Keerthi Vihar <viharpeddagopu@gmail.com>
2026-02-02 23:15:26 +0530

    Change API endpoint from '/api/bookings' to '/bookings'

--- backend/src/main/java/com/SPATanalytics/backend/controller/BookingController.java
+++ backend/src/main/java/com/SPATanalytics/backend/controller/BookingController.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/bookings")
+@RequestMapping("/bookings")
 public class BookingController {
 
     private final BookingService bookingService;
