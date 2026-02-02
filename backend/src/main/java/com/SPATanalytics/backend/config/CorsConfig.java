d8d97992536461b7eeb9c0d0e28d12bd220852ec
Peddagopu Keerthi Vihar <viharpeddagopu@gmail.com>
2026-02-02 22:39:21 +0530

    Update CORS configuration for allowed origins new

--- backend/src/main/java/com/SPATanalytics/backend/config/CorsConfig.java
+++ backend/src/main/java/com/SPATanalytics/backend/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "spat-analytics-frontend-7onr.vercel.app",
+                        "https://spat-analytics-frontend-7onr-q0guzwdzi-viharpeddagopus-projects.vercel.app",
                         "http://localhost:5173"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
